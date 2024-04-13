1f2f6851570a0a2bfe5b1ebe926e35bd12eed5a3
Adam Dzurilla <adamdzurilla19@gmail.com>
2024-04-13 22:14:20 +0200

    clean code, saving best border and best avg correctness

--- src/speech/train_speech_model.py
+++ src/speech/train_speech_model.py
@@ -8,32 +8,28 @@ import time
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def calculate_test_score(ll_target, P_target, ll_non_target, P_non_target):
+def calculate_test_score(ll_target, ll_non_target):
     return sum(ll_target) - sum(ll_non_target)
 
 def evaluate_test_data(test_data, 
-                       Ws_target, MUs_target, COVs_target, P_target, 
-                       Ws_non_target, MUs_non_target, COVs_non_target, P_non_target):
+                       Ws_target, MUs_target, COVs_target, 
+                       Ws_non_target, MUs_non_target, COVs_non_target):
     score=[]
     for i in range(len(test_data)):
         tst = test_data[i]
         ll_target = logpdf_gmm(tst, Ws_target, MUs_target, COVs_target)
         ll_non_target = logpdf_gmm(tst, Ws_non_target, MUs_non_target, COVs_non_target)
-        score.append(calculate_test_score(ll_target, P_target, ll_non_target, P_non_target))
+        score.append(calculate_test_score(ll_target, ll_non_target))
     return score
 
 def simulate_run(score_borders, iterations):
     _, train_target = wav16khz2mfcc('../data/target_train', augment=True)
     _, train_non_target = wav16khz2mfcc('../data/non_target_train', augment=True)
-    test_target_keys, test_target = wav16khz2mfcc('../data/target_dev')
-    test_non_target_keys, test_non_target = wav16khz2mfcc('../data/non_target_dev')
+    _, test_target = wav16khz2mfcc('../data/target_dev')
+    _, test_non_target = wav16khz2mfcc('../data/non_target_dev')
 
     train_target = np.vstack(train_target)
     train_non_target = np.vstack(train_non_target)
-
-    # Define uniform a-priori probabilities of classes:
-    P_target = 0.5
-    P_non_target = 1 - P_target
 
     # Train and test with GMM models with diagonal covariance matrices
     # Decide for number of gaussian mixture components used for the target and non target models
@@ -49,10 +45,9 @@ def simulate_run(score_borders, iterations):
     Ws_target = np.ones(M_target) / M_target
     Ws_non_target = np.ones(M_non_target) / M_non_target
 
-    max_avg_correctness = 97.5
+    max_avg_correctness = np.loadtxt('max_avg_correctness.txt')
     for iter in range(iterations):
         print("Run:", iter+1)
-        # Initialize mean vectors, covariance matrices and weights of mixture components
         # Initialize mean vectors to randomly selected data points from corresponding class
         MUs_target = train_target[randint(1, len(train_target), M_target)]
 
@@ -64,7 +59,7 @@ def simulate_run(score_borders, iterations):
         for jj in range(30):
             [Ws_target, MUs_target, COVs_target, TTL_target] = train_gmm(train_target, Ws_target, MUs_target, COVs_target)
             [Ws_non_target, MUs_non_target, COVs_non_target, TTL_non_target] = train_gmm(train_non_target, Ws_non_target, MUs_non_target, COVs_non_target)
-            if jj % 10 == 0:
+            if (jj+1) % 5 == 0:
                 print('Iteration:', jj, ' Total log-likelihoods:', TTL_target, 'for target;', TTL_non_target, 'for non targets.')
         end_time = time.time()
         execution_time = end_time - start_time
@@ -72,12 +67,12 @@ def simulate_run(score_borders, iterations):
 
         # Now run recognition for all target test utterances
         score_target = evaluate_test_data(test_target, 
-                                    Ws_target, MUs_target, COVs_target, P_target, 
-                                    Ws_non_target, MUs_non_target, COVs_non_target, P_non_target)
+                                    Ws_target, MUs_target, COVs_target, 
+                                    Ws_non_target, MUs_non_target, COVs_non_target)
 
         score_non_target = evaluate_test_data(test_non_target, 
-                                    Ws_target, MUs_target, COVs_target, P_target, 
-                                    Ws_non_target, MUs_non_target, COVs_non_target, P_non_target)
+                                    Ws_target, MUs_target, COVs_target, 
+                                    Ws_non_target, MUs_non_target, COVs_non_target)
         
         for border in score_borders:
             correct_targets = sum(1 for s in score_target if s > border)
@@ -100,13 +95,15 @@ def simulate_run(score_borders, iterations):
                 np.savetxt('MUs_non_target.txt', MUs_non_target)
                 np.savetxt('COVs_target.txt', COVs_target)
                 np.savetxt('COVs_non_target.txt', COVs_non_target)
+                np.savetxt('border.txt', border)
+                np.savetxt('max_avg_correctness.txt', crc_avg)
                 max_avg_correctness = crc_avg
             print()
 
 def main():
-    score_borders = [0, 200, 400]
+    score_borders = [-200, 0, 200, 400, 600]
     # Run n iterations of simulation to get distinct correctnesses
-    num_iterations = 5
+    num_iterations = 10
     simulate_run(score_borders, num_iterations)
 
 main()
